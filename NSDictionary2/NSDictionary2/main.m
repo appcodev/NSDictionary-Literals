14866925ff89c92949ceed6294c34db44b330b35
Chalermchon Samana <appcodev@gmail.com>
2013-03-28 01:04:18 +0700

    +dic2

---
+++ NSDictionary2/NSDictionary2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSDictionary2
+//
+//  Created by Chalermchon Samana on 3/28/13.
+//  Copyright (c) 2013 Onzondev Innovation Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

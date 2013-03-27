0959994878c717443c63b5060be5ee048a000c12
Chalermchon Samana <appcodev@gmail.com>
2013-03-28 01:41:51 +0700

    ok

--- NSDictionary2/NSDictionary2/main.m
+++ NSDictionary2/NSDictionary2/main.m
@@ -13,8 +13,22 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSDictionary *aDic = @{@"key1": @"object1",@"key2":@"object2"};
+        NSArray *aArray = @[@"object1",@"object2",@"object3"];
+        NSString *aString = @"my string";
+        NSNumber *aNumber = @99;
+        
+        NSDictionary *allData = @{@"dic data": aDic,@"array data":aArray,@"string data":aString,@"number data":aNumber};
+        
+        NSLog(@"dic data : %@",allData);
+        
+        //all key
+        NSArray *keys = [allData allKeys];
+        NSArray *values = [allData allValues];
+        
+        NSLog(@"all key : %@",keys);
+        
+        NSLog(@"all values : %@",values);
         
     }
     return 0;
